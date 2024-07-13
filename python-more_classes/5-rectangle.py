baf9779a1f82bc6816402d8b2d67429354df8ac1
Shyakammik <s.mike@alustudent.com>
2024-07-13 12:32:09 -0700

    Newcode 5

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -44,3 +44,38 @@ class Rectangle:
         """Return the area of the reactangle
         """
         return self.__height * self.__width
+
+    def perimeter(self):
+        """This returns the perimeter of the rectangle
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__height + self.__width)
+
+    def __del__(self):
+        '''
+        Detects the deletion of an instance
+        and returns bye when it's deleted
+        '''
+        print("Bye rectangle...")
+
+    def __str__(self):
+        '''Print the rectangle with using #'''
+        if self.__width == 0 or self.__height == 0:
+            return ''
+        else:
+            rect = ''
+            for i in range(self.__height):
+                for x in range(self.__width):
+                    rect = rect + '#'
+
+                rect += '\n'
+            return rect[:-1]
+
+    def __repr__(self):
+        """
+        should return a string representation of the rectangle
+        to be able to recreate a new instance
+        """
+        return "Rectangle({}, {})".format(self.__width, self.__height)
