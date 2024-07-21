cd3008b6c97031751186a64a0b5fd07e4d6fbac3
Shyakammik <s.mike@alustudent.com>
2024-07-21 07:57:42 -0700

    T1

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,7 @@
--- show all privileges of data users 
+-- Show all privileges for data users
+
+-- For user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- For user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
